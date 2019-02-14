a7b6e04d9187bc77430bc892080f68b9a9cb6a58
Alessandro Maraio <am963@sussex.ac.uk>
2019-02-14 14:52:36 +0000

    Now fitting Gassians with Taylor expansions

--- GaussianFit.py
+++ GaussianFit.py
@@ -15,23 +15,31 @@ def ChiSqFunc(Measured,Fitted,Errors):
     ProbChiSq = (1.0 - scistats.chi2.cdf(ChiSquared,len(Measured)-2) )  * 100.0
     return ChiSquared, ReducedChiSq, ProbChiSq
 
-def gauss(x,amplitude,mean,sigma,offset):
-    return amplitude*np.exp(-((x-mean)**2.0)/(2.0*(sigma**2.0))) + offset
+def Gauss(x,amplitude,mean,sigma,a):
+    return (amplitude/(np.sqrt(2*np.pi) * sigma )) *np.exp(-((x-mean)**2.0)/(2.0*(sigma**2.0))) + a
+
+def GaussLinear(x,amplitude,mean,sigma,a,b):
+    return (amplitude/(np.sqrt(2*np.pi) * sigma ))*np.exp(-((x-mean)**2.0)/(2.0*(sigma**2.0))) + a + b*x
+
+def GaussQuad(x,amplitude,mean,sigma,a,b,c):
+    return (amplitude/(np.sqrt(2*np.pi) * sigma ))*np.exp(-((x-mean)**2.0)/(2.0*(sigma**2.0))) + a + b*x + c*x**2.0    
 
 source = "Barium133-2HrRun_009_eh_1"
 df = pd.read_table(source+ ".dat", sep="\s+",names = ['channel number','count number'])
 
 df['count errors'] = np.sqrt(df['count number'])
 
-data = df[(565<=df['channel number']) & (df['channel number']<=575)]
-
-GaussModel = Model(gauss)
+data = df[(1565<=df['channel number']) & (df['channel number']<=1582)]
+'''
+----------------------------------------------------------------------------------------------------------
+'''
+GaussModel = Model(Gauss)
 Params = Parameters()
 
-Params.add('amplitude',value=1,vary=True)
-Params.add('mean',value=570,vary=True)
+Params.add('amplitude',value=max(df['count number']),vary=True)
+Params.add('mean',value=1572,vary=True)
 Params.add('sigma',value=1,vary=True)
-Params.add('offset',value=1,vary=True)
+Params.add('a',value=1,vary=True)
 
 FitResult = GaussModel.fit(data['count number'],params=Params,x=data['channel number'])
 print(FitResult.best_values)
@@ -39,6 +47,49 @@ FitResult.plot(yerr=data['count errors'])
 
 thingy = ChiSqFunc(list(data['count number']),list(FitResult.best_fit),list(data['count errors']))
 print(thingy)
+
+plt.plot(data['channel number'],data['count number'])
+#plt.show()
+'''
+----------------------------------------------------------------------------------------------------------
+'''
+GaussModel2 = Model(GaussLinear)
+Params2 = Parameters()
+
+Params2.add('amplitude',value=max(df['count number']),vary=True)
+Params2.add('mean',value=1570,vary=True)
+Params2.add('sigma',value=1,vary=True)
+Params2.add('a',value=1,vary=True)
+Params2.add('b',value=1,vary=True)
+
+FitResult2 = GaussModel2.fit(data['count number'],params=Params2,x=data['channel number'])
+print(FitResult2.best_values)
+FitResult2.plot(yerr=data['count errors'])
+
+thingy2 = ChiSqFunc(list(data['count number']),list(FitResult2.best_fit),list(data['count errors']))
+print(thingy2)
+
+plt.plot(data['channel number'],data['count number'])
+#plt.show()
+'''
+----------------------------------------------------------------------------------------------------------
+'''
+GaussModel3 = Model(GaussQuad)
+Params3 = Parameters()
+
+Params3.add('amplitude',value=max(df['count number']),vary=True)
+Params3.add('mean',value=1570,vary=True)
+Params3.add('sigma',value=1,vary=True)
+Params3.add('a',value=1,vary=True)
+Params3.add('b',value=1,vary=True)
+Params3.add('c',value=1,vary=True)
+
+FitResult3 = GaussModel3.fit(data['count number'],params=Params3,x=data['channel number'])
+print(FitResult3.best_values)
+FitResult3.plot(yerr=data['count errors'])
+
+thingy3 = ChiSqFunc(list(data['count number']),list(FitResult3.best_fit),list(data['count errors']))
+print(thingy3)
 
 plt.plot(data['channel number'],data['count number'])
 plt.show()
