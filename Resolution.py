b190847aa64993cbb099bd707d793652507fce5d
Alessandro Maraio <am963@sussex.ac.uk>
2019-02-28 17:24:52 +0000

    Resolution with fit file

--- Resolution.py
+++ Resolution.py
@@ -7,6 +7,9 @@ sns.set()
 import scipy.stats as scistats
 
 
+def ResolutionFit(E,a,b):
+    return a/E + b
+
 FitDF = pd.read_csv('Gamma_Peak_Stats_and_Params.csv', names=['Element', 'Fit type', 'Peak number',	'Peak type', 'Energy (keV)', 'Resolution', 'Min', 'Max', 'Mean', 'A', 'Sigma', 'Error' ,'a', 'b', 'chisq', 'Reduced chisq', 'Probchisq'],usecols=list(range(1,18)))
 
 FitDF = FitDF[FitDF['Fit type'] == 'Linear']
@@ -14,18 +17,27 @@ FitDF = FitDF[FitDF['Peak type'] == 'Zoom']
 
 FitDF['Energy (keV)'] = FitDF['Energy (keV)'].astype(float)
 FitDF['Resolution'] = FitDF['Resolution'].astype(float).multiply(100)
+SodiumDF = FitDF[(FitDF['Energy (keV)'] == 511)]
+FitDF = FitDF[~(FitDF['Energy (keV)'] == 511)]
+
 
 DataBa = FitDF[FitDF['Element'] == 'Barium133']
 DataCo = FitDF[FitDF['Element'] == 'Cobalt60']
 DataNa = FitDF[FitDF['Element'] == 'Sodium22']
 
 
+Fit, Errors = sciopt.curve_fit(ResolutionFit,FitDF['Energy (keV)'],FitDF['Resolution'])
+
 TitleFont = {'size':'20', 'color':'black', 'weight':'bold'} 
 AxTitleFont = {'size':'18'}
 
-plt.plot(DataBa['Energy (keV)'],DataBa['Resolution'],'ro',label="Barium-133",markersize=10.5)
-plt.plot(DataCo['Energy (keV)'],DataCo['Resolution'],'bo',label="Cobalt-60",markersize=10.5)
-plt.plot(DataNa['Energy (keV)'],DataNa['Resolution'],'yo',label="Sodium-22",markersize=10.5)
+Energies = np.linspace(55,1400,14000)
+plt.plot(Energies,ResolutionFit(Energies,*Fit),label="Fit")
+
+plt.errorbar(DataBa['Energy (keV)'],DataBa['Resolution'],fmt="o",color='red',label="Barium-133",markersize=7,yerr=(7/DataBa['Energy (keV)']),elinewidth=3,capsize=5,capthick=3,marker='o')
+plt.errorbar(DataCo['Energy (keV)'],DataCo['Resolution'],fmt="o",color='blue',label="Cobalt-60",markersize=7,yerr=(7/DataCo['Energy (keV)']),elinewidth=3,capsize=5,capthick=3,marker='o')
+plt.errorbar(DataNa['Energy (keV)'],DataNa['Resolution'],fmt="o",color='yellow',label="Sodium-22",markersize=7,yerr=(7/DataNa['Energy (keV)']),elinewidth=3,capsize=5,capthick=3,marker='o')
+plt.errorbar(SodiumDF['Energy (keV)'],SodiumDF['Resolution'],fmt="o",color='yellow',label="",markersize=7,yerr=(7/SodiumDF['Energy (keV)']),elinewidth=3,capsize=5,capthick=3,marker='o')
 plt.xlabel('Peak Energy [keV]',**AxTitleFont)
 plt.ylabel('Energy Resolution [%]',**AxTitleFont)
 plt.title('Energy Resolution as a Function of Gamma Energy',**TitleFont)
